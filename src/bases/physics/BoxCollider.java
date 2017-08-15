7b552f51c060c7059d6261e363243cb5bfe55b5a
sup3r3d <dddinh24@gmail.com>
2017-08-15 18:09:34 +0700

    Add BoxCollider class

---
+++ src/bases/physics/BoxCollider.java
@@ -0,0 +1,42 @@
+package bases.physics;
+
+import bases.GameObject;
+
+public class BoxCollider extends GameObject {
+    private float width;
+    private float height;
+
+    public BoxCollider(float x, float y, float width, float height) {
+        super();
+        this.position.set(x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    public BoxCollider(float width, float height) {
+        this(0,0, width, height);
+    }
+
+    public boolean intersects(BoxCollider other) {
+        return this.left() <= other.right() &&
+                this.right() >= other.left() &&
+                this.bottom() >= other.top() &&
+                this.top() <= other.bottom();
+    }
+
+    private float top() {
+        return this.position.y - this.height / 2;
+    }
+
+    private float bottom() {
+        return this.position.y + this.height / 2;
+    }
+
+    private float right() {
+        return this.position.x + this.width / 2;
+    }
+
+    private float left() {
+        return this.position.x - this.width / 2;
+    }
+}
